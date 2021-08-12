7265dc4ad623baed662f4a71b2e3149cf36d836e
Sho <sho.nozaki417@gmail.com>
2021-08-12 12:59:46 -0700

    set the currentItem at drag start

--- src/components/Column/Column.tsx
+++ src/components/Column/Column.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './Column.css'
 
 const data = {
@@ -10,12 +10,23 @@ const data = {
 const Column = () => {
 	const [list, setList] = useState(data)
 
+	const currentItem = useRef()
+
+	const handleDragStart = (e: any) => {
+		// console.log(e.target)
+		currentItem.current = e.target
+	}
+
 	return (
 		<div className="column">
 			<div className="heading">{list.title}</div>
 			{list.items.map(item => {
 			  return (	
-					<div draggable className="column-item">
+					<div 
+					className="column-item"
+					draggable
+					onDragStart={handleDragStart} 
+					>
 						<p>Item {item}</p>
 					</div>	
 			  )
